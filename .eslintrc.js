74afb9ff55176cba83a1c456a0bc057291b00a11
Bret Hudson <bret@brethudson.com>
2022-11-26 15:44:05 -0800

    Modified ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
 	extends: [
 		'@vercel/style-guide/eslint/browser',
 		'@vercel/style-guide/eslint/node',
-	].map((config) => require.resolve(config)),
+	].map(config => require.resolve(config)),
 	env: {
 		es2022: true,
 	},
@@ -11,6 +11,19 @@ module.exports = {
 		sourceType: 'script',
 	},
 	ignorePatterns: ['node_modules/'],
-	rules: {}
-  }
-  
+	rules: {
+		curly: 0,
+		'no-bitwise': 0,
+		'no-multi-assign': 0,
+		'no-implicit-coercion': ['error', {
+			'allow': ['~', '+'],
+		}],
+
+		// Temporary
+		'no-console': 0,
+		'no-constant-condition': 0,
+		'no-extend-native': 0,
+		'no-lonely-if': 0,
+		'no-unused-vars': 0,
+	},
+};
