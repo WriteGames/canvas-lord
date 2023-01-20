63ad56b0291cd49bec1d70f71ee8162c789b2ef3
Bret Hudson <bret@brethudson.com>
2023-01-20 15:47:14 -0800

    Added mouse events to Jest tests

--- src/input.spec.ts
+++ src/input.spec.ts
@@ -45,7 +45,7 @@ const emulateKeyUp = (input: Input, key: Key): void => {
 	} as KeyboardEvent);
 };
 
-describe('input', () => {
+describe('input / keyboard', () => {
 	test('should not have any keyboard state on a key at init', () => {
 		const leftPressed = input.keyPressed('ArrowLeft');
 		expect(leftPressed).toEqual(false);
@@ -121,6 +121,106 @@ describe('input', () => {
 
 			test('should not be held after release frame', () => {
 				const leftHeld = input.keyCheck('ArrowLeft');
+				expect(leftHeld).toEqual(false);
+			});
+		});
+	});
+});
+
+const emulateMouseDown = (input: Input, button: number): void => {
+	input.onMouseDown({
+		preventDefault: () => {
+			/**/
+		},
+		button,
+	} as MouseEvent);
+};
+
+const emulateMouseUp = (input: Input, button: number): void => {
+	input.onMouseUp({
+		preventDefault: () => {
+			/**/
+		},
+		button,
+	} as MouseEvent);
+};
+
+describe('input / mouse', () => {
+	test('should not have any mouse state at init', () => {
+		const leftPressed = input.mousePressed();
+		expect(leftPressed).toEqual(false);
+		const leftHeld = input.mouseCheck();
+		expect(leftHeld).toEqual(false);
+		const leftReleased = input.mouseReleased();
+		expect(leftReleased).toEqual(false);
+	});
+
+	// mouse down
+	describe('on mousedown', () => {
+		beforeEach(() => {
+			emulateMouseDown(input, 0);
+		});
+
+		describe('first frame', () => {
+			test('should register mousedown events', () => {
+				const leftPressed = input.mousePressed();
+				expect(leftPressed).toEqual(true);
+			});
+
+			test('should be held on press frame', () => {
+				const leftHeld = input.mouseCheck();
+				expect(leftHeld).toEqual(true);
+			});
+		});
+
+		describe('second frame', () => {
+			beforeEach(() => {
+				input.update();
+			});
+
+			test('should only be pressed for a single frame', () => {
+				const leftPressed = input.mousePressed();
+				expect(leftPressed).toEqual(false);
+			});
+
+			test('should be held after press frame', () => {
+				const leftHeld = input.mouseCheck();
+				expect(leftHeld).toEqual(true);
+			});
+		});
+	});
+
+	// mouse up
+	describe('on mouseup', () => {
+		beforeEach(() => {
+			emulateMouseDown(input, 0);
+			emulateMouseUp(input, 0);
+		});
+
+		describe('first frame', () => {
+			test('should register mouseup events', () => {
+				const leftReleased = input.mouseReleased();
+				expect(leftReleased).toEqual(true);
+			});
+
+			test('should not be held on release frame', () => {
+				const leftHeld = input.mouseCheck();
+				expect(leftHeld).toEqual(false);
+			});
+		});
+
+		describe('second frame', () => {
+			beforeEach(() => {
+				input.update();
+			});
+
+			test('should only be released for a single frame', () => {
+				const leftReleased = input.mouseReleased();
+				expect(leftReleased).toEqual(false);
+			});
+
+			test('should not be held after release frame', () => {
+				const leftHeld = input.mouseCheck();
 				expect(leftHeld).toEqual(false);
 			});
 		});
