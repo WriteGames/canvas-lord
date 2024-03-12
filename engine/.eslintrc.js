7a5223476423ff37dbe22305a1d4ade7f9e0d05f
Bret Hudson <bret@brethudson.com>
2024-03-12 11:17:11 -0700

    Updated package.json files - Updated `transpile` script - Fixed outdated paths

--- engine/.eslintrc.js
+++ engine/.eslintrc.js
@@ -1,6 +1,6 @@
 const { resolve } = require('node:path');
 
-const project = resolve(process.cwd(), 'src/tsconfig.json');
+const project = resolve(process.cwd(), 'engine/tsconfig.json');
 
 const baseESLintConfig = require('../.eslintrc');
 
