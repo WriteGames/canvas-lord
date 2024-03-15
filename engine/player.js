b262299bc874a5b918bd51c8b292d050f75b133e
Bret Hudson <bret@brethudson.com>
2024-03-15 09:21:38 -0700

    Remove the duplicate Entity class from player.js

--- engine/player.js
+++ engine/player.js
@@ -1,43 +1,5 @@
 import * as Components from './bin/util/components.js';
-
-class Entity {
-	components = new Map();
-
-	constructor(x, y) {
-		this.addComponent(Components.pos2D);
-
-		this.x = x;
-		this.y = y;
-	}
-
-	addComponent(component) {
-		// TODO: we'll want to make sure we use a deepCopy
-		this.components.set(component, Components.copyObject(component));
-		return this.component(component);
-	}
-
-	component(component) {
-		return this.components.get(component);
-	}
-
-	get x() {
-		return this.component(Components.pos2D)[0];
-	}
-	set x(val) {
-		this.component(Components.pos2D)[0] = val;
-	}
-
-	get y() {
-		return this.component(Components.pos2D)[1];
-	}
-	set y(val) {
-		this.component(Components.pos2D)[1] = val;
-	}
-
-	update() {}
-
-	render() {}
-}
+import { Entity } from './bin/util/entity.js';
 
 export const leftKeys = ['ArrowLeft', 'a', 'A'];
 export const rightKeys = ['ArrowRight', 'd', 'D'];
