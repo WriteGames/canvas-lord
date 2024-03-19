dd28a4ed6cd186091174caf2446537ee711529e2
Bret Hudson <bret@brethudson.com>
2024-03-18 17:42:39 -0700

    Updated Camera type

--- engine/util/camera.ts
+++ engine/util/camera.ts
@@ -1,5 +1,10 @@
 import type { V2 } from './math';
 
+export interface Camera {
+	[0]: number;
+	[1]: number;
+}
+
 export class Camera extends Array {
 	constructor(x: V2[0], y: V2[1]) {
 		super();
@@ -7,14 +12,14 @@ export class Camera extends Array {
 	}
 
 	get x(): number {
-		return this[0] as number;
+		return this[0];
 	}
 	set x(val) {
 		this[0] = val;
 	}
 
 	get y(): number {
-		return this[1] as number;
+		return this[1];
 	}
 	set y(val) {
 		this[1] = val;
