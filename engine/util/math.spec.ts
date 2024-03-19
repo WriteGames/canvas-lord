65574d45b9513310fb3b1884cac38a8804d42de8
Bret Hudson <bret@brethudson.com>
2024-03-18 18:20:28 -0700

    Test existing functions in math.ts

---
+++ engine/util/math.spec.ts
@@ -0,0 +1,114 @@
+import { addPos, subPos, scalePos, posEqual } from './math.js';
+import { describe, expect, test } from 'vitest';
+
+describe('Vector operations', () => {
+	describe('Vector & Vector', () => {
+		describe('addPos(a, b)', () => {
+			describe('when a & b are both the same length', () => {
+				test('V2 + V2 should result in a V2 sum', () => {
+					const sum = addPos([4, 5], [0, 1]);
+					expect(sum).toHaveLength(2);
+					expect(sum).toEqual([4, 6]);
+				});
+
+				test('V3 + V3 should result in a V3 sum', () => {
+					const sum = addPos([4, 5, 6], [0, 1, 2]);
+					expect(sum).toHaveLength(3);
+					expect(sum).toEqual([4, 6, 8]);
+				});
+
+				test('V4 + V4 should result in a V4 sum', () => {
+					const sum = addPos([4, 5, 6, 7], [0, 1, 2, 3]);
+					expect(sum).toHaveLength(4);
+					expect(sum).toEqual([4, 6, 8, 10]);
+				});
+			});
+
+			describe('when a is longer than b', () => {
+				test("sum should be of length a, treating b's missing elements as 0", () => {
+					const sum = addPos([2, 3, 4], [0, 1]);
+					expect(sum).toHaveLength(3);
+					expect(sum).toEqual([2, 4, 4]);
+				});
+			});
+
+			describe('when a is shorter than b', () => {
+				test('sum should be of length a, truncating extra elements from b', () => {
+					const sum = addPos([3, 4], [0, 1, 2]);
+					expect(sum).toHaveLength(2);
+					expect(sum).toEqual([3, 5]);
+				});
+			});
+		});
+
+		describe('subPos(a, b)', () => {
+			describe('when a & b are both the same length', () => {
+				test('V2 - V2 should result in a V2 difference', () => {
+					const diff = subPos([4, 5], [0, 1]);
+					expect(diff).toHaveLength(2);
+					expect(diff).toEqual([4, 4]);
+				});
+
+				test('V3 - V3 should result in a V3 difference', () => {
+					const diff = subPos([4, 5, 6], [0, 1, 2]);
+					expect(diff).toHaveLength(3);
+					expect(diff).toEqual([4, 4, 4]);
+				});
+
+				test('V4 - V4 should result in a V4 difference', () => {
+					const diff = subPos([4, 5, 6, 7], [0, 1, 2, 3]);
+					expect(diff).toHaveLength(4);
+					expect(diff).toEqual([4, 4, 4, 4]);
+				});
+			});
+
+			describe('when a is longer than b', () => {
+				test("difference should be of length a, treating b's missing elements as 0", () => {
+					const diff = subPos([2, 3, 4], [0, 1]);
+					expect(diff).toHaveLength(3);
+					expect(diff).toEqual([2, 2, 4]);
+				});
+			});
+
+			describe('when a is shorter than b', () => {
+				test('difference should be of length a, truncating extra elements from b', () => {
+					const diff = subPos([3, 4], [0, 1, 2]);
+					expect(diff).toHaveLength(2);
+					expect(diff).toEqual([3, 3]);
+				});
+			});
+		});
+
+		describe('posEqual(a, b)', () => {
+			test('should return true if both vectors are of equal length and have the same valued elements', () => {
+				const isEqual = posEqual([1, 2, 3], [1, 2, 3]);
+				expect(isEqual).toEqual(true);
+			});
+
+			test('should return false if the vectors do not match', () => {
+				const isEqual = posEqual([1, 2, 3], [1, 2, 4]);
+				expect(isEqual).toEqual(false);
+			});
+
+			test('should return false if vector a is longer, even if the shared elements match', () => {
+				const isEqual = posEqual([1, 2, 3], [1, 2]);
+				expect(isEqual).toEqual(false);
+			});
+
+			test('should return false if vector b is longer, even if the shared elements match', () => {
+				const isEqual = posEqual([1, 2], [1, 2, 3]);
+				expect(isEqual).toEqual(false);
+			});
+		});
+	});
+
+	describe('Vector & Scalar', () => {
+		describe('scalePos(p, s)', () => {
+			test('should return a vector of equal length scaled by a scalar', () => {
+				const scaled = scalePos([1, 2, 3], 5);
+				expect(scaled).toHaveLength(3);
+				expect(scaled).toEqual([5, 10, 15]);
+			});
+		});
+	});
+});
