f7ac1e47cdcfb7f3530df1b2f337956cb9f623e9
Bret Hudson <bret@brethudson.com>
2024-04-03 15:55:46 -0700

    Make sure canvases are using `pixel` option :)

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -4,11 +4,11 @@ export default function Page() {
 	return (
 		<div>
 			<h3>Basic Input</h3>
-			<Canvas id="game-1" src="game1" width={320} height={180} />
+			<Canvas id="game-1" src="game1" width={320} height={180} pixel />
 			<h3>Coyote Time</h3>
-			<Canvas id="game-2" src="game2" width={320} height={180} />
+			<Canvas id="game-2" src="game2" width={320} height={180} pixel />
 			<h3>Input Buffering</h3>
-			<Canvas id="game-3" src="game3" width={320} height={180} />
+			<Canvas id="game-3" src="game3" width={320} height={180} pixel />
 		</div>
 	);
 }
