c657e2527133fdd7862d00c6bafaf4bb664124ad
Bret Hudson <bret@brethudson.com>
2025-01-19 17:41:51 -0800

    Added Random class

---
+++ engine/bin/util/random.js
@@ -0,0 +1,35 @@
+"use strict";
+const xorShift32 = (random) => {
+    let x = random.seed;
+    x ^= x << 13;
+    x ^= x >>> 17;
+    x ^= x << 5;
+    return (random.seed = x >>> 0);
+};
+class Random {
+    constructor(seed = Date.now()) {
+        this.seed = seed;
+    }
+    #next() {
+        return Number(xorShift32(this) / 0xffffffff);
+    }
+    float(n = 0) {
+        return this.#next() * n;
+    }
+    int(n) {
+        return Math.floor(this.#next() * n);
+    }
+    range(a, b) {
+        return this.float(b - a) + a;
+    }
+    bool() {
+        return this.int(2) > 0;
+    }
+    sign() {
+        return this.bool() ? 1 : -1;
+    }
+    angle() {
+        return this.float(360);
+    }
+}
+//# sourceMappingURL=random.js.map
---
+++ engine/util/random.ts
@@ -0,0 +1,45 @@
+const xorShift32 = (random: Random) => {
+	let x = random.seed;
+	x ^= x << 13;
+	x ^= x >>> 17;
+	x ^= x << 5;
+	return (random.seed = x >>> 0);
+};
+
+interface Random {
+	seed: number;
+}
+
+class Random {
+	constructor(seed: number = Date.now()) {
+		this.seed = seed;
+	}
+
+	#next() {
+		return Number(xorShift32(this) / 0xffffffff);
+	}
+
+	float(n: number = 0) {
+		return this.#next() * n;
+	}
+
+	int(n: number) {
+		return Math.floor(this.#next() * n);
+	}
+
+	range(a: number, b: number) {
+		return this.float(b - a) + a;
+	}
+
+	bool() {
+		return this.int(2) > 0;
+	}
+
+	sign() {
+		return this.bool() ? 1 : -1;
+	}
+
+	angle() {
+		return this.float(360);
+	}
+}
