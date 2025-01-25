ccb96d6b8ba50ea4b4f08558ba6e7b716a764426
Bret Hudson <bret@brethudson.com>
2025-01-25 15:30:11 -0800

    Added AnimatedSprite

--- engine/bin/util/graphic.js
+++ engine/bin/util/graphic.js
@@ -199,6 +199,95 @@ export class Sprite extends Graphic {
         Draw.image(ctx, this, x, y, sourceX, sourceY, sourceW, sourceH);
     }
 }
+export class AnimatedSprite extends Graphic {
+    asset;
+    // TODO(bret): remove these and allow Draw.image to make them optional
+    frame = 0;
+    frameId = 0;
+    frameW = 0;
+    frameH = 0;
+    framesPerRow = 0;
+    sourceX = 0;
+    sourceY = 0;
+    sourceW;
+    sourceH;
+    color;
+    blend;
+    animations = new Map();
+    currentAnimation;
+    get width() {
+        return this.imageSrc.width;
+    }
+    get w() {
+        return this.width;
+    }
+    get height() {
+        return this.imageSrc.height;
+    }
+    get h() {
+        return this.height;
+    }
+    get imageSrc() {
+        if (!this.asset.image)
+            throw new Error("asset.image hasn't loaded yet");
+        return this.asset.image;
+    }
+    constructor(asset, frameW, frameH) {
+        super();
+        this.asset = asset;
+        if (frameW === undefined || frameH === undefined)
+            throw new Error('please supply frameW/frameH');
+        this.frame = 0;
+        this.frameId = 0;
+        this.frameW = frameW;
+        this.frameH = frameH;
+        this.sourceX = 0;
+        this.sourceY = 0;
+        this.sourceW = asset.image?.width;
+        this.sourceH = asset.image?.height;
+    }
+    add(name, frames, frameRate, loop = true) {
+        const animation = {
+            name,
+            frames,
+            frameRate,
+            loop,
+        };
+        this.animations.set(name, animation);
+    }
+    play(name) {
+        this.inc = 0;
+        this.currentAnimation =
+            name !== undefined ? this.animations.get(name) : name;
+    }
+    stop() {
+        this.play();
+    }
+    centerOrigin() {
+        this.offsetX = -this.frameW >> 1;
+        this.offsetY = -this.frameH >> 1;
+        this.originX = -this.frameW >> 1;
+        this.originY = -this.frameH >> 1;
+    }
+    inc = 0;
+    update() {
+        if (this.currentAnimation) {
+            const { frames, frameRate } = this.currentAnimation;
+            this.frame = Math.floor(this.inc / frameRate) % frames.length;
+            this.frameId = frames[this.frame];
+            ++this.inc;
+        }
+    }
+    render(ctx, camera) {
+        const { frameId, frameW, frameH } = this;
+        this.framesPerRow = this.imageSrc.width / frameW;
+        const sourceX = (frameId % this.framesPerRow) * frameW;
+        const sourceY = Math.floor(frameId / this.framesPerRow) * frameH;
+        const x = this.x - camera.x * this.scrollX + (this.parent?.x ?? 0);
+        const y = this.y - camera.y * this.scrollY + (this.parent?.y ?? 0);
+        Draw.image(ctx, this, x, y, sourceX, sourceY, frameW, frameH);
+    }
+}
 // TODO(bret): Could have this extend from Sprite maybe, or a new parent class... hmm...
 export class NineSlice extends Graphic {
     asset;
--- engine/util/graphic.ts
+++ engine/util/graphic.ts
@@ -310,6 +310,121 @@ export class Sprite extends Graphic implements ISpriteLike {
 	}
 }
 
+interface Animation {
+	name: string;
+	frames: number[];
+	frameRate: number;
+	loop: boolean;
+}
+
+export class AnimatedSprite extends Graphic implements ISpriteLike {
+	asset: SpriteAsset;
+
+	// TODO(bret): remove these and allow Draw.image to make them optional
+	frame: number = 0;
+	frameId: number = 0;
+	frameW: number = 0;
+	frameH: number = 0;
+	framesPerRow: number = 0;
+
+	sourceX: number = 0;
+	sourceY: number = 0;
+	sourceW: number | undefined;
+	sourceH: number | undefined;
+
+	color?: string;
+	blend?: boolean;
+
+	animations: Map<string, Animation> = new Map();
+	currentAnimation?: Animation;
+
+	get width() {
+		return this.imageSrc.width;
+	}
+	get w() {
+		return this.width;
+	}
+
+	get height() {
+		return this.imageSrc.height;
+	}
+	get h() {
+		return this.height;
+	}
+
+	get imageSrc(): HTMLImageElement {
+		if (!this.asset.image) throw new Error("asset.image hasn't loaded yet");
+		return this.asset.image;
+	}
+
+	constructor(asset: SpriteAsset, frameW: number, frameH: number) {
+		super();
+		this.asset = asset;
+
+		if (frameW === undefined || frameH === undefined)
+			throw new Error('please supply frameW/frameH');
+
+		this.frame = 0;
+		this.frameId = 0;
+		this.frameW = frameW;
+		this.frameH = frameH;
+
+		this.sourceX = 0;
+		this.sourceY = 0;
+		this.sourceW = asset.image?.width;
+		this.sourceH = asset.image?.height;
+	}
+
+	add(name: string, frames: number[], frameRate: number, loop = true) {
+		const animation: Animation = {
+			name,
+			frames,
+			frameRate,
+			loop,
+		};
+		this.animations.set(name, animation);
+	}
+
+	play(name?: string) {
+		this.inc = 0;
+		this.currentAnimation =
+			name !== undefined ? this.animations.get(name) : name;
+	}
+
+	stop() {
+		this.play();
+	}
+
+	centerOrigin() {
+		this.offsetX = -this.frameW >> 1;
+		this.offsetY = -this.frameH >> 1;
+		this.originX = -this.frameW >> 1;
+		this.originY = -this.frameH >> 1;
+	}
+
+	inc = 0;
+
+	update() {
+		if (this.currentAnimation) {
+			const { frames, frameRate } = this.currentAnimation;
+			this.frame = Math.floor(this.inc / frameRate) % frames.length;
+			this.frameId = frames[this.frame];
+			++this.inc;
+		}
+	}
+
+	render(ctx: CanvasRenderingContext2D, camera: Camera) {
+		const { frameId, frameW, frameH } = this;
+		this.framesPerRow = this.imageSrc.width / frameW;
+
+		const sourceX = (frameId % this.framesPerRow) * frameW;
+		const sourceY = Math.floor(frameId / this.framesPerRow) * frameH;
+		const x = this.x - camera.x * this.scrollX + (this.parent?.x ?? 0);
+		const y = this.y - camera.y * this.scrollY + (this.parent?.y ?? 0);
+		Draw.image(ctx, this, x, y, sourceX, sourceY, frameW, frameH);
+	}
+}
+
 // TODO(bret): Could have this extend from Sprite maybe, or a new parent class... hmm...
 export class NineSlice extends Graphic implements ISpriteLike {
 	asset: SpriteAsset;
