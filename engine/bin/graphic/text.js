b68b644a5b979a816a3d7326ed1efc44ed79f911
Bret Hudson <bret@brethudson.com>
2025-02-02 02:39:24 -0800

    Text (Graphic) now caches its width/height

--- engine/bin/graphic/text.js
+++ engine/bin/graphic/text.js
@@ -4,34 +4,119 @@ import { Vec2 } from '../math/index.js';
 import { Draw } from '../util/draw.js';
 const textCanvas = document.createElement('canvas');
 const textCtx = textCanvas.getContext('2d');
+// TODO(bret): Make this a global ?
+const defaultTextOptions = {
+    color: 'white', // what do we want for default?
+    type: 'fill',
+    font: 'sans-serif',
+    size: 10,
+    align: 'left',
+    // TODO(bret): check if this is the default we want :/
+    baseline: 'top',
+};
 export class Text extends Graphic {
     str;
-    color = 'white'; // what do we want for default?
-    type = 'fill';
-    font = 'sans-serif';
-    size = 10;
-    align = 'left';
-    // TODO(bret): check if this is the default we want :/
-    baseline = 'top';
-    constructor(str, x, y) {
+    #count;
+    #color;
+    #type;
+    #font;
+    #size;
+    #align;
+    #baseline;
+    #invalided = true;
+    get count() {
+        return this.#count;
+    }
+    set count(value) {
+        this.#invalided = true;
+        this.#count = value;
+    }
+    get color() {
+        return this.#color;
+    }
+    set color(value) {
+        this.#invalided = true;
+        this.#color = value;
+    }
+    get type() {
+        return this.#type;
+    }
+    set type(value) {
+        this.#invalided = true;
+        this.#type = value;
+    }
+    get font() {
+        return this.#font;
+    }
+    set font(value) {
+        this.#invalided = true;
+        this.#font = value;
+    }
+    get size() {
+        return this.#size;
+    }
+    set size(value) {
+        this.#invalided = true;
+        this.#size = value;
+    }
+    get align() {
+        return this.#align;
+    }
+    set align(value) {
+        this.#invalided = true;
+        this.#align = value;
+    }
+    get baseline() {
+        return this.#baseline;
+    }
+    set baseline(value) {
+        this.#invalided = true;
+        this.#baseline = value;
+    }
+    get width() {
+        this.#revalidate();
+        return this.#metrics.width;
+    }
+    get height() {
+        this.#revalidate();
+        return (this.#metrics.actualBoundingBoxAscent +
+            this.#metrics.actualBoundingBoxDescent);
+    }
+    #metrics;
+    constructor(str, x, y, options = {}) {
         super(x, y);
         this.str = str;
+        const _options = Object.assign({}, defaultTextOptions, options);
+        this.color = _options.color;
+        this.type = _options.type;
+        this.font = _options.font;
+        this.size = _options.size;
+        this.align = _options.align;
+        this.baseline = _options.baseline;
+        this.#revalidate();
     }
     centerOrigin() {
+        this.#revalidate();
+        this.offsetX = -this.width / 2;
+        this.offsetY = -this.height / 2;
+    }
+    #revalidate() {
+        if (!this.#invalided)
+            return;
+        this.#invalided = false;
         if (!textCtx)
             throw new Error();
-        const { font = 'sans-serif', size = 10, align = 'left', baseline = 'top', // TODO(bret): check if this is the default we want :/
-        // count,
-         } = this;
+        // TODO(bret): count
+        const { font, size, align, baseline, count } = this;
         textCtx.save();
         const _size = typeof size === 'number' ? `${size}px` : size;
         textCtx.font = `${_size} ${font}`;
         textCtx.textAlign = align;
         textCtx.textBaseline = baseline;
-        const metrics = textCtx.measureText(this.str);
-        const height = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
-        this.offsetX = -metrics.width / 2;
-        this.offsetY = -height / 2;
+        let _str = this.str;
+        if (count !== undefined)
+            _str = _str.slice(0, count);
+        this.#metrics = textCtx.measureText(_str);
         textCtx.restore();
     }
     render(ctx, camera = Vec2.zero) {
--- engine/graphic/text.ts
+++ engine/graphic/text.ts
@@ -1,41 +1,152 @@
 /* Canvas Lord v0.5.0 */
 import { Graphic } from './graphic.js';
-import type { ImageAsset } from '../core/asset-manager.js';
-import type { Entity } from '../core/entity.js';
-import type { Input } from '../core/input.js';
 import { Vec2 } from '../math/index.js';
 import type { Camera } from '../util/camera.js';
-import { moveCanvas, Draw } from '../util/draw.js';
-import { Random } from '../util/random.js';
+import { Draw } from '../util/draw.js';
+import type { CSSColor } from '../util/types.js';
 
 const textCanvas = document.createElement('canvas');
 const textCtx = textCanvas.getContext('2d');
+
+interface TextOptions {
+	color: CSSColor;
+	type: 'fill' | 'stroke';
+	font: string;
+	size: number;
+	align: CanvasTextAlign;
+	baseline: CanvasTextBaseline;
+}
+
+// TODO(bret): Make this a global ?
+const defaultTextOptions: TextOptions = {
+	color: 'white', // what do we want for default?
+	type: 'fill',
+	font: 'sans-serif',
+	size: 10,
+	align: 'left',
+	// TODO(bret): check if this is the default we want :/
+	baseline: 'top',
+};
+
 export class Text extends Graphic {
 	str: string;
 
-	color: string = 'white'; // what do we want for default?
-	type: 'fill' | 'stroke' = 'fill';
-	font: string = 'sans-serif';
-	size: number = 10;
-	align: CanvasTextAlign = 'left';
-	// TODO(bret): check if this is the default we want :/
-	baseline: CanvasTextBaseline = 'top';
+	#count?: number;
+	#color!: TextOptions['color'];
+	#type!: TextOptions['type'];
+	#font!: TextOptions['font'];
+	#size!: TextOptions['size'];
+	#align!: TextOptions['align'];
+	#baseline!: TextOptions['baseline'];
 
-	constructor(str: string, x: number, y: number) {
+	#invalided: Boolean = true;
+
+	get count() {
+		return this.#count;
+	}
+	set count(value) {
+		this.#invalided = true;
+		this.#count = value;
+	}
+
+	get color() {
+		return this.#color;
+	}
+	set color(value) {
+		this.#invalided = true;
+		this.#color = value;
+	}
+
+	get type() {
+		return this.#type;
+	}
+	set type(value) {
+		this.#invalided = true;
+		this.#type = value;
+	}
+
+	get font() {
+		return this.#font;
+	}
+	set font(value) {
+		this.#invalided = true;
+		this.#font = value;
+	}
+
+	get size() {
+		return this.#size;
+	}
+	set size(value) {
+		this.#invalided = true;
+		this.#size = value;
+	}
+
+	get align() {
+		return this.#align;
+	}
+	set align(value) {
+		this.#invalided = true;
+		this.#align = value;
+	}
+
+	get baseline() {
+		return this.#baseline;
+	}
+	set baseline(value) {
+		this.#invalided = true;
+		this.#baseline = value;
+	}
+
+	get width() {
+		this.#revalidate();
+		return this.#metrics.width;
+	}
+	get height() {
+		this.#revalidate();
+		return (
+			this.#metrics.actualBoundingBoxAscent +
+			this.#metrics.actualBoundingBoxDescent
+		);
+	}
+
+	#metrics!: TextMetrics;
+
+	constructor(
+		str: string,
+		x: number,
+		y: number,
+		options: Partial<TextOptions> = {},
+	) {
 		super(x, y);
 		this.str = str;
+
+		const _options = Object.assign({}, defaultTextOptions, options);
+
+		this.color = _options.color;
+		this.type = _options.type;
+		this.font = _options.font;
+		this.size = _options.size;
+		this.align = _options.align;
+		this.baseline = _options.baseline;
+
+		this.#revalidate();
 	}
 
 	centerOrigin(): void {
+		this.#revalidate();
+
+		this.offsetX = -this.width / 2;
+		this.offsetY = -this.height / 2;
+	}
+
+	#revalidate() {
+		if (!this.#invalided) return;
+		this.#invalided = false;
+
 		if (!textCtx) throw new Error();
 
-		const {
-			font = 'sans-serif',
-			size = 10,
-			align = 'left',
-			baseline = 'top', // TODO(bret): check if this is the default we want :/
-			// count,
-		} = this;
+		// TODO(bret): count
+		const { font, size, align, baseline, count } = this;
 
 		textCtx.save();
 		const _size = typeof size === 'number' ? `${size}px` : size;
@@ -44,13 +155,10 @@ export class Text extends Graphic {
 		textCtx.textAlign = align;
 		textCtx.textBaseline = baseline;
 
-		const metrics = textCtx.measureText(this.str);
+		let _str = this.str;
+		if (count !== undefined) _str = _str.slice(0, count);
+		this.#metrics = textCtx.measureText(_str);
 
-		const height =
-			metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
-
-		this.offsetX = -metrics.width / 2;
-		this.offsetY = -height / 2;
 		textCtx.restore();
 	}
 
