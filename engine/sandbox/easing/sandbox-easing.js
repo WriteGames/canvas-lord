e50be7dd3fbed76c75cb95d3d0481423733bc39b
Bret Hudson <bret@brethudson.com>
2025-03-13 11:03:38 -0700

    Add easing sandbox

---
+++ engine/sandbox/easing/sandbox-easing.js
@@ -0,0 +1,199 @@
+import { Entity, Scene } from '../../bin/canvas-lord.js';
+import { init } from '../sandbox.js';
+import { Ease as FPEase } from './flashpunk-ease.js';
+import { Ease as EASEYBOY } from '../../bin/util/ease.js';
+import { Sprite } from '../../bin/graphic/sprite.js';
+import { Text } from '../../bin/graphic/text.js';
+import { Draw } from '../../bin/util/draw.js';
+
+const startX = 150;
+const endX = 550;
+const spanX = endX - startX;
+
+const colors = [
+	'red',
+	'lime',
+	'magenta',
+	'yellow',
+	'cyan',
+	'pink',
+	'white',
+	'orange',
+	'silver',
+	'gold',
+	'green',
+];
+
+let color = 0;
+
+class EaseEntity extends Entity {
+	constructor(x, y, ease) {
+		super(x, y + 8);
+		if (ease) this.ease = FPEase[ease];
+		else this.ease = (x) => x;
+
+		this.graphic = Sprite.createRect(16, 16, colors[color]);
+		this.graphic.centerOO();
+	}
+
+	setT(t) {
+		this.x = startX + spanX * this.ease(t);
+	}
+}
+
+class EaseEntity2 extends Entity {
+	constructor(x, y, ease) {
+		super(x, y + 8);
+		if (ease) this.ease = EASEYBOY[ease];
+		else this.ease = (x) => x;
+
+		this.graphic = Sprite.createRect(20, 20, colors[color + 1]);
+		this.graphic.centerOO();
+	}
+
+	setT(t) {
+		this.x = startX + spanX * this.ease(t);
+	}
+}
+
+const toPrefix = (k) => k.slice(0, 3);
+
+class BaseScene extends Scene {
+	frame = 0;
+	frameDur = 60 * 3;
+	easeEntities = [];
+
+	xPos = startX;
+	yPos = 12;
+	yPad = 20;
+
+	update(...args) {
+		super.update(...args);
+
+		const t = Math.min(1, this.frame / this.frameDur);
+		this.easeEntities.forEach((e) => e.setT(t));
+
+		if (++this.frame > this.frameDur * 1.25) {
+			this.frame = 0;
+		}
+	}
+
+	render(gameCtx, ...args) {
+		const ctx = this.ctx ?? gameCtx;
+		const options = {
+			color: 'white',
+		};
+		const { height } = this.engine;
+		Draw.line(ctx, options, startX, 0, startX, height);
+		Draw.line(ctx, options, endX, 0, endX, height);
+
+		super.render(gameCtx, ...args);
+	}
+}
+
+class EaseScene extends BaseScene {
+	constructor(engine) {
+		super(engine);
+
+		color = 0;
+
+		this.addEase(null);
+
+		let lastKey = 'null';
+		const keys = Object.keys(FPEase);
+		for (let i = 0; i < keys.length; ++i) {
+			const prefix = toPrefix(keys[i]);
+			if (keys[i].endsWith('OutIn')) continue;
+			if (lastKey !== prefix) {
+				this.yPos += this.yPad;
+				lastKey = prefix;
+				++color;
+			}
+			this.addEase(keys[i]);
+		}
+	}
+
+	addEase(ease) {
+		const e = new EaseEntity(this.xPos, this.yPos, ease);
+		this.easeEntities.push(e);
+		this.addEntity(e);
+
+		const text = new Text(ease, 12, this.yPos);
+		this.addGraphic(text);
+
+		this.yPos += this.yPad;
+	}
+}
+
+class EaseScene2 extends BaseScene {
+	constructor(engine, include) {
+		super(engine);
+
+		color = 0;
+
+		this.addEase(null);
+
+		const includePrefixes = include.map(toPrefix);
+
+		let lastKey = 'null';
+		const keys = Object.keys(EASEYBOY);
+		for (let i = 0; i < keys.length; ++i) {
+			const prefix = toPrefix(keys[i]);
+			if (!includePrefixes.includes(prefix)) continue;
+
+			if (lastKey !== prefix) {
+				this.yPos += this.yPad;
+				lastKey = prefix;
+				++color;
+			}
+			this.addEase(keys[i]);
+		}
+	}
+
+	addEase(ease) {
+		const e2 = new EaseEntity2(this.xPos, this.yPos, ease);
+		this.easeEntities.push(e2);
+		this.addEntity(e2);
+
+		const e = new EaseEntity(this.xPos, this.yPos, ease);
+		this.easeEntities.push(e);
+		this.addEntity(e);
+
+		const text = new Text(ease, 12, this.yPos);
+		this.addGraphic(text);
+
+		this.yPos += this.yPad;
+	}
+}
+
+const gameSettings = {
+	backgroundColor: '#202020',
+	gameLoopSettings: {
+		updateMode: 'always',
+	},
+};
+
+const onStart = (game) => {
+	window.requestAnimationFrame(() => {
+		game.render();
+	});
+};
+
+const gameOptions = {
+	gameSettings,
+	onStart,
+};
+
+init({
+	games: [
+		// init.game('easing', EaseScene, gameOptions),
+		init.game('easing-2', EaseScene2, {
+			...gameOptions,
+			sceneArgs: [['quad', 'cube', 'quart', 'quint', 'sine']],
+		}),
+		init.game('easing-3', EaseScene2, {
+			...gameOptions,
+			sceneArgs: [['bounce', 'circ', 'expo', 'back']],
+		}),
+	],
+});
--- engine/util/ease.ts
+++ engine/util/ease.ts
@@ -1,126 +1,71 @@
-/* eslint-disable no-param-reassign -- we assign to t in many of the funtions */
+const linear = (t: number): number => t;
+const quad = (t: number): number => t * t;
+const cube = (t: number): number => t * t * t;
+const quart = (t: number): number => t * t * t * t;
+const quint = (t: number): number => t * t * t * t * t;
+const HALF_PI: number = Math.PI / 2;
+const sine = (t: number): number => -Math.cos(HALF_PI * t) + 1;
+const B_DENOM = 2.75;
+const B1: number = 1 / B_DENOM;
+const B2: number = 2 / B_DENOM;
+const B3: number = 1.5 / B_DENOM;
+const B4: number = 2.5 / B_DENOM;
+const B5: number = 2.25 / B_DENOM;
+const B6: number = 2.625 / B_DENOM;
+const bounce = (t: number): number => {
+	const _t = 1 - t;
+	if (_t < B1) return 1 - 7.5625 * _t * _t;
+	if (_t < B2) return 1 - (7.5625 * (_t - B3) * (_t - B3) + 0.75);
+	if (_t < B4) return 1 - (7.5625 * (_t - B5) * (_t - B5) + 0.9375);
+	return 1 - (7.5625 * (_t - B6) * (_t - B6) + 0.984375);
+};
+const circ = (t: number): number => -(Math.sqrt(1 - t * t) - 1);
+const expo = (t: number): number => Math.pow(2, 10 * (t - 1));
+const back = (t: number): number => t * t * (2.70158 * t - 1.70158);
 
-/*
-Copyright (c) 2010 Chevy Ray Johnston
+type EaseFunc = (t: number) => number;
+type EaseWrap = (func: EaseFunc) => EaseFunc;
 
- Permission is hereby granted, free of charge, to any person
- obtaining a copy of this software and associated documentation
- files (the "Software"), to deal in the Software without
- restriction, including without limitation the rights to use,
- copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the
- Software is furnished to do so, subject to the following
- conditions:
+const easeOut: EaseWrap = (func) => (t) => 1 - func(1 - t);
+const easeInOut: EaseWrap = (func) => (t) =>
+	0.5 * (t <= 0.5 ? func(2 * t) : 2 - func(2 * (1 - t)));
+const easeOutIn: EaseWrap = (func) => (t) =>
+	0.5 * (t <= 0.5 ? 1 - func(1 - t * 2) : func(t * 2 - 1) + 1);
 
- The above copyright notice and this permission notice shall be
- included in all copies or substantial portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- OTHER DEALINGS IN THE SOFTWARE.
-*/
-// From https://github.com/useflashpunk/FlashPunk/blob/master/net/flashpunk/utils/Ease.as
-
-const PI: number = Math.PI;
-const PI2: number = Math.PI / 2;
-const B1: number = 1 / 2.75;
-const B2: number = 2 / 2.75;
-const B3: number = 1.5 / 2.75;
-const B4: number = 2.5 / 2.75;
-const B5: number = 2.25 / 2.75;
-const B6: number = 2.625 / 2.75;
+type EaseMatrix<T extends string> = Record<
+	`${T}In` | `${T}Out` | `${T}InOut` | `${T}OutIn`,
+	EaseFunc
+>;
+const generateMatrix = <T extends string>(
+	name: T,
+	func: EaseFunc,
+): EaseMatrix<T> => {
+	return {
+		[`${name}In`]: func,
+		[`${name}Out`]: easeOut(func),
+		[`${name}InOut`]: easeInOut(func),
+		[`${name}OutIn`]: easeOutIn(func),
+	} as EaseMatrix<T>;
+};
 
 export const Ease = Object.freeze({
-	quadIn: (t: number): number => t * t,
-	quadOut: (t: number): number => -t * (t - 2),
-	quadInOut: (t: number): number => {
-		return t <= 0.5 ? t * t * 2 : 1 - --t * t * 2;
-	},
-
-	cubeIn: (t: number): number => t * t * t,
-	cubeOut: (t: number): number => 1 + --t * t * t,
-	cubeInOut: (t: number): number => {
-		return t <= 0.5 ? t * t * t * 4 : 1 + --t * t * t * 4;
-	},
-
-	quartIn: (t: number): number => t * t * t * t,
-	quartOut: (t: number): number => 1 - (t -= 1) * t * t * t,
-	quartInOut: (t: number): number => {
-		return t <= 0.5
-			? t * t * t * t * 8
-			: (1 - (t = t * 2 - 2) * t * t * t) / 2 + 0.5;
-	},
-
-	quintIn: (t: number): number => t * t * t * t * t,
-	quintOut: (t: number): number => (t = t - 1) * t * t * t * t + 1,
-	quintInOut: (t: number): number => {
-		return (t *= 2) < 1
-			? (t * t * t * t * t) / 2
-			: ((t -= 2) * t * t * t * t + 2) / 2;
-	},
-
-	sineIn: (t: number): number => -Math.cos(PI2 * t) + 1,
-	sineOut: (t: number): number => Math.sin(PI2 * t),
-	sineInOut: (t: number): number => -Math.cos(PI * t) / 2 + 0.5,
-
-	bounceIn: (t: number): number => {
-		t = 1 - t;
-		if (t < B1) return 1 - 7.5625 * t * t;
-		if (t < B2) return 1 - (7.5625 * (t - B3) * (t - B3) + 0.75);
-		if (t < B4) return 1 - (7.5625 * (t - B5) * (t - B5) + 0.9375);
-		return 1 - (7.5625 * (t - B6) * (t - B6) + 0.984375);
-	},
-	bounceOut: (t: number): number => {
-		if (t < B1) return 7.5625 * t * t;
-		if (t < B2) return 7.5625 * (t - B3) * (t - B3) + 0.75;
-		if (t < B4) return 7.5625 * (t - B5) * (t - B5) + 0.9375;
-		return 7.5625 * (t - B6) * (t - B6) + 0.984375;
-	},
-	bounceInOut: (t: number): number => {
-		if (t < 0.5) {
-			t = 1 - t * 2;
-			if (t < B1) return (1 - 7.5625 * t * t) / 2;
-			if (t < B2) return (1 - (7.5625 * (t - B3) * (t - B3) + 0.75)) / 2;
-			if (t < B4)
-				return (1 - (7.5625 * (t - B5) * (t - B5) + 0.9375)) / 2;
-			return (1 - (7.5625 * (t - B6) * (t - B6) + 0.984375)) / 2;
-		}
-		t = t * 2 - 1;
-		if (t < B1) return (7.5625 * t * t) / 2 + 0.5;
-		if (t < B2) return (7.5625 * (t - B3) * (t - B3) + 0.75) / 2 + 0.5;
-		if (t < B4) return (7.5625 * (t - B5) * (t - B5) + 0.9375) / 2 + 0.5;
-		return (7.5625 * (t - B6) * (t - B6) + 0.984375) / 2 + 0.5;
-	},
-
-	circIn: (t: number): number => -(Math.sqrt(1 - t * t) - 1),
-	circOut: (t: number): number => Math.sqrt(1 - (t - 1) * (t - 1)),
-	circInOut: (t: number): number => {
-		return t <= 0.5
-			? (Math.sqrt(1 - t * t * 4) - 1) / -2
-			: (Math.sqrt(1 - (t * 2 - 2) * (t * 2 - 2)) + 1) / 2;
-	},
-
-	expoIn: (t: number): number => Math.pow(2, 10 * (t - 1)),
-	expoOut: (t: number): number => -Math.pow(2, -10 * t) + 1,
-	expoInOut: (t: number): number => {
-		return t < 0.5
-			? Math.pow(2, 10 * (t * 2 - 1)) / 2
-			: (-Math.pow(2, -10 * (t * 2 - 1)) + 2) / 2;
-	},
-
-	backIn: (t: number): number => t * t * (2.70158 * t - 1.70158),
-	backOut: (t: number): number => 1 - --t * t * (-2.70158 * t - 1.70158),
-	backInOut: (t: number): number => {
-		t *= 2;
-		if (t < 1) return (t * t * (2.70158 * t - 1.70158)) / 2;
-		t--;
-		return (1 - --t * t * (-2.70158 * t - 1.70158)) / 2 + 0.5;
-	},
+	linear,
+	quad,
+	cube,
+	quart,
+	quint,
+	sine,
+	bounce,
+	circ,
+	expo,
+	back,
+	...generateMatrix('quad', quad),
+	...generateMatrix('cube', cube),
+	...generateMatrix('quart', quart),
+	...generateMatrix('quint', quint),
+	...generateMatrix('sine', sine),
+	...generateMatrix('bounce', bounce),
+	...generateMatrix('circ', circ),
+	...generateMatrix('expo', expo),
+	...generateMatrix('back', back),
 });
-
-/* eslint-enable no-param-reassign -- we assign to t in many of the funtions */
